e4331c43a994d620d62e78c9a5fe7d8b99c8ffdb
David Brungardt <brungardtdb@gmail.com>
2018-01-15 21:06:36 -0700

    Initial commit

---
+++ frmGalaxyHotel.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmGalaxyHotel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnOccupancyRate = New System.Windows.Forms.Button()
+        Me.lstGalaxyHotelOccupancy = New System.Windows.Forms.ListBox()
+        Me.lblOccupancyRate = New System.Windows.Forms.Label()
+        Me.lblVacantRooms = New System.Windows.Forms.Label()
+        Me.lblOccupiedRooms = New System.Windows.Forms.Label()
+        Me.lblTotalRooms = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("ISOCPEUR", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(67, 43)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(173, 37)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Galaxy Hotel"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("ISOCPEUR", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 89)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(263, 27)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Occupancy Rate Calculator"
+        '
+        'btnOccupancyRate
+        '
+        Me.btnOccupancyRate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOccupancyRate.Location = New System.Drawing.Point(70, 134)
+        Me.btnOccupancyRate.Name = "btnOccupancyRate"
+        Me.btnOccupancyRate.Size = New System.Drawing.Size(177, 41)
+        Me.btnOccupancyRate.TabIndex = 2
+        Me.btnOccupancyRate.Text = "Calculate Occupancy"
+        Me.btnOccupancyRate.UseVisualStyleBackColor = True
+        '
+        'lstGalaxyHotelOccupancy
+        '
+        Me.lstGalaxyHotelOccupancy.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstGalaxyHotelOccupancy.FormattingEnabled = True
+        Me.lstGalaxyHotelOccupancy.ItemHeight = 20
+        Me.lstGalaxyHotelOccupancy.Location = New System.Drawing.Point(56, 193)
+        Me.lstGalaxyHotelOccupancy.Name = "lstGalaxyHotelOccupancy"
+        Me.lstGalaxyHotelOccupancy.Size = New System.Drawing.Size(204, 144)
+        Me.lstGalaxyHotelOccupancy.TabIndex = 3
+        '
+        'lblOccupancyRate
+        '
+        Me.lblOccupancyRate.AutoSize = True
+        Me.lblOccupancyRate.Font = New System.Drawing.Font("ISOCPEUR", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOccupancyRate.Location = New System.Drawing.Point(79, 478)
+        Me.lblOccupancyRate.Name = "lblOccupancyRate"
+        Me.lblOccupancyRate.Size = New System.Drawing.Size(215, 21)
+        Me.lblOccupancyRate.TabIndex = 4
+        Me.lblOccupancyRate.Text = "Total Occupancy Rate is __.__%"
+        Me.lblOccupancyRate.Visible = False
+        '
+        'lblVacantRooms
+        '
+        Me.lblVacantRooms.AutoSize = True
+        Me.lblVacantRooms.Font = New System.Drawing.Font("ISOCPEUR", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblVacantRooms.Location = New System.Drawing.Point(79, 445)
+        Me.lblVacantRooms.Name = "lblVacantRooms"
+        Me.lblVacantRooms.Size = New System.Drawing.Size(164, 21)
+        Me.lblVacantRooms.TabIndex = 4
+        Me.lblVacantRooms.Text = "Number of vacant rooms:"
+        Me.lblVacantRooms.Visible = False
+        '
+        'lblOccupiedRooms
+        '
+        Me.lblOccupiedRooms.AutoSize = True
+        Me.lblOccupiedRooms.Font = New System.Drawing.Font("ISOCPEUR", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOccupiedRooms.Location = New System.Drawing.Point(79, 414)
+        Me.lblOccupiedRooms.Name = "lblOccupiedRooms"
+        Me.lblOccupiedRooms.Size = New System.Drawing.Size(174, 21)
+        Me.lblOccupiedRooms.TabIndex = 4
+        Me.lblOccupiedRooms.Text = "Number of occupied rooms:"
+        Me.lblOccupiedRooms.Visible = False
+        '
+        'lblTotalRooms
+        '
+        Me.lblTotalRooms.AutoSize = True
+        Me.lblTotalRooms.Font = New System.Drawing.Font("ISOCPEUR", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalRooms.Location = New System.Drawing.Point(79, 383)
+        Me.lblTotalRooms.Name = "lblTotalRooms"
+        Me.lblTotalRooms.Size = New System.Drawing.Size(161, 21)
+        Me.lblTotalRooms.TabIndex = 4
+        Me.lblTotalRooms.Text = "Total number of rooms: "
+        Me.lblTotalRooms.Visible = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(317, 24)
+        Me.MenuStrip1.TabIndex = 5
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(101, 22)
+        Me.ClearToolStripMenuItem.Text = "&Clear"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(101, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'frmGalaxyHotel
+        '
+        Me.AcceptButton = Me.btnOccupancyRate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MediumPurple
+        Me.ClientSize = New System.Drawing.Size(317, 541)
+        Me.Controls.Add(Me.lblTotalRooms)
+        Me.Controls.Add(Me.lblOccupiedRooms)
+        Me.Controls.Add(Me.lblVacantRooms)
+        Me.Controls.Add(Me.lblOccupancyRate)
+        Me.Controls.Add(Me.lstGalaxyHotelOccupancy)
+        Me.Controls.Add(Me.btnOccupancyRate)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmGalaxyHotel"
+        Me.Text = "Galaxy Hotel Occupancy Rate"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnOccupancyRate As Button
+    Friend WithEvents lstGalaxyHotelOccupancy As ListBox
+    Friend WithEvents lblOccupancyRate As Label
+    Friend WithEvents lblVacantRooms As Label
+    Friend WithEvents lblOccupiedRooms As Label
+    Friend WithEvents lblTotalRooms As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ frmGalaxyHotel.vb
@@ -0,0 +1,166 @@
+﻿'Program Name: Galaxy Hotel Occupancy Calculator
+' Author:       David Brungardt
+'Date:          12/10/2017
+' Purpose:      The Galaxy Hotel Occupancy Calculator program
+'               allows the user to enter the number of occupied
+'               and unoccupied rooms for each floor (up to 7) in
+'               the Galaxy Hotel. It then calculates the occupancy
+'               rate, and displays the total rooms, occupied rooms
+'               unoccupied rooms, and the occupancy rate.
+
+Option Strict On
+
+
+Public Class frmGalaxyHotel
+    Private Sub frmGalaxyHotel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblHeading.Click
+
+    End Sub
+
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+        ' the mnuClear click event clears the ListBox object and
+        ' hides the occupancy rate label, total rooms label, 
+        ' occupied rooms label, and unoccupied rooms label. It 
+        ' also enables the calculate occupancy button.
+
+        lstGalaxyHotelOccupancy.Items.Clear()
+        lblOccupancyRate.Visible = False
+        lblTotalRooms.Visible = False
+        lblOccupiedRooms.Visible = False
+        lblVacantRooms.Visible = False
+        btnOccupancyRate.Enabled = True
+        btnOccupancyRate.Focus()
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        ' the mnuExit clik event closes the window and exits the application
+
+        Close()
+
+    End Sub
+
+    Private Sub btnOccupancyRate_Click(sender As Object, e As EventArgs) Handles btnOccupancyRate.Click
+        'This loop allows the user to enter the number of occupied roomas and 
+        ' the number of vacant rooms at the Galaxy Hotel for up to 7 floors.
+        ' It then calculates and displays the occupancy rate by dividing the 
+        'total occupied rooms by the total vacant rooms.
+
+        'Declare and initialize variables
+
+        Dim strOccupiedRooms As String
+        Dim decOccupiedRooms As Decimal
+        Dim decTotalOccupiedRooms As Decimal = 0D
+        Dim strVacantRooms As String
+        Dim decVacantRooms As Decimal
+        Dim decTotalVacantRooms As Decimal = 0D
+        Dim decTotalRooms As Decimal = 0D
+        Dim decOccupancyRate As Decimal = 0D
+
+        Dim strInputMessageOccupied As String = "Enter a value for the number of occupied rooms on floor #"
+        Dim strInputMessageVacant As String = "Enter a value for the number of vacant rooms on floor #"
+        Dim strInputHeading As String = "Occupancy"
+        Dim strNormalMessageOccupied As String = "Enter a value for the number of occupied rooms on floor #"
+        Dim strNormalMessageVacant As String = "Enter a value for the number of vacant rooms on floor #"
+        Dim strNonNumericErrorOccupied As String = "Enter a numeric value for the number of occupied rooms on floor #"
+        Dim strNonNumericErrorVacant As String = "Error - Enter a numeric value for the number of vacant rooms on floor #"
+        Dim strNegativeErrorOccupied As String = "Error - Enter a positive value for the number of occupied rooms on floor #"
+        Dim strNegativeErrorVacant As String = "Error - Enter a positive value for the number of occupied rooms on floor #"
+
+        ' Declare and initialize loop variables
+        Dim strCancelClicked As String = ""
+        Dim intMaxNumberOfEntriesOccupied As Integer = 7
+        Dim intMaxNumberOfEntriesVacant As Integer = 7
+        Dim intNumberOfEntriesOccupied As Integer = 1
+        Dim intNumberOfEntriesVacant As Integer = 1
+
+        'This loop allows the user to enter the number of occupied
+        ' and vacant rooms on each floor of the Galaxy Hotel. 
+        ' The loop terminates when the user has entered 7 values
+        ' for occupied and 7 values for vacant or when the user
+        ' clicks the Cancel button or the Close button in the InputBox.
+
+        strOccupiedRooms = InputBox(strInputMessageOccupied & intNumberOfEntriesOccupied, strInputHeading, "")
+        strVacantRooms = InputBox(strInputMessageVacant & intNumberOfEntriesVacant, strInputHeading, "")
+
+
+        Do Until intNumberOfEntriesOccupied > intMaxNumberOfEntriesOccupied And intNumberOfEntriesVacant > intMaxNumberOfEntriesVacant Or strOccupiedRooms = strCancelClicked Or strVacantRooms = strCancelClicked
+
+            If IsNumeric(strOccupiedRooms) Then
+                decOccupiedRooms = Convert.ToDecimal(strOccupiedRooms)
+
+                If decOccupiedRooms > 0 Then
+
+                    If IsNumeric(strVacantRooms) Then
+                        decVacantRooms = Convert.ToDecimal(strVacantRooms)
+
+                        If decVacantRooms > 0 Then
+                            lstGalaxyHotelOccupancy.Items.Add(decOccupiedRooms & " Occupied  " & decVacantRooms & "  Vacant")
+                            decTotalOccupiedRooms += decOccupiedRooms
+                            intNumberOfEntriesOccupied += 1
+                            decTotalVacantRooms += decVacantRooms
+                            intNumberOfEntriesVacant += 1
+
+                        Else
+                            strInputMessageVacant = strNegativeErrorVacant
+                        End If
+
+                    Else
+                        strInputMessageVacant = strNonNumericErrorVacant
+                    End If
+
+                Else
+                    strInputMessageOccupied = strNegativeErrorOccupied
+                End If
+
+            Else
+                strInputMessageOccupied = strNonNumericErrorOccupied
+            End If
+
+            If intNumberOfEntriesOccupied <= intMaxNumberOfEntriesOccupied Then
+                strOccupiedRooms = InputBox(strInputMessageOccupied & intNumberOfEntriesOccupied, strInputHeading, "")
+                If intNumberOfEntriesVacant <= intMaxNumberOfEntriesVacant Then
+                    strVacantRooms = InputBox(strInputMessageVacant & intNumberOfEntriesVacant, strInputHeading, "")
+                End If
+            End If
+        Loop
+
+        'Calculates and displays total occupied rooms, total vacant rooms, and occupancy rate for Galaxy Hotel
+
+        If intNumberOfEntriesOccupied > 1 Then
+            If intNumberOfEntriesVacant > 1 Then
+                lblOccupiedRooms.Visible = True
+                lblVacantRooms.Visible = True
+                lblTotalRooms.Visible = True
+                lblOccupancyRate.Visible = True
+                decTotalRooms = decTotalOccupiedRooms + decTotalVacantRooms
+                decOccupancyRate = (decTotalOccupiedRooms / decTotalRooms) * 100D
+                lblOccupiedRooms.Text = "Occupied Rooms: " &
+                    decTotalOccupiedRooms
+                lblVacantRooms.Text = "Vacant Rooms: " &
+                    decTotalVacantRooms
+                lblTotalRooms.Text = "Total Rooms: " &
+                    decTotalRooms
+                lblOccupancyRate.Text = "Occupancy Rate: " &
+                    decOccupancyRate.ToString("F1") & "%"
+            Else
+                MsgBox("No room value entered")
+
+            End If
+
+        Else
+            MsgBox("No room value entered")
+        End If
+
+        ' Disables the btnOccupancyRate
+        btnOccupancyRate.Enabled = False
+
+
+
+
+    End Sub
+End Class
